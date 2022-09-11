c684b9e0169c88287fe7fe5c44d8379c7322555d
Flow-matic <jonathonvinson22@gmail.com>
2022-09-11 11:11:47 +0000

    Added basic checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -35,7 +35,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51LgWnFBO1x3Ln70ij2Q9Do4gRZap9EQjvv1VaANb5kvImF8VLzrHWrgweTl7PDUTvG95oYoxQV6v3JHBtplo9TAZ00WSrF8qXw',
+        'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
     }
 
