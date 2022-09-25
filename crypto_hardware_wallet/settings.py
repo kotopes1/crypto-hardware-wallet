f0bf0796df5c0edc8d3631723dca237ff1a060e4
Flow-matic <jonathonvinson22@gmail.com>
2022-09-25 10:46:10 +0000

    Updeted crispy forms because check-out was showing error

--- crypto_hardware_wallet/settings.py
+++ crypto_hardware_wallet/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-(1=r+*h#psvq6-rlzx@3#y99-c&*pu4lf63fgz!q__oo=(jdt^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
