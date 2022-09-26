4804271e42cbec5e9ca6097d15f19b2488660743
Flow-matic <jonathonvinson22@gmail.com>
2022-09-26 13:57:14 +0000

    Removed secret key and set debug

--- crypto_hardware_wallet/settings.py
+++ crypto_hardware_wallet/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(1=r+*h#psvq6-rlzx@3#y99-c&*pu4lf63fgz!q__oo=(jdt^'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['crypto-hardware-wallets.herokuapp.com', 'localhost']
 
