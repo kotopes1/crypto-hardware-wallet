3942c62464c3c2be4f14b21f02259638addcd8fb
Flow-matic <jonathonvinson22@gmail.com>
2022-10-10 19:35:57 +0000

    Added django secret key generator and variables to gitpod

--- crypto_hardware_wallet/settings.py
+++ crypto_hardware_wallet/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'dfc%b%t4ww++a@$d5hsb-7kz=!!#hkak3_x8#l_hhuk2wifp4e')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
