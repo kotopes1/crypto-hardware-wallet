f635cc49043bd65e71d637c650aa7e6f3b895339
Flow-matic <jonathonvinson22@gmail.com>
2022-10-17 18:15:59 +0000

    Updated code in urls.py and views.py while creating error404 page

--- crypto_hardware_wallet/urls.py
+++ crypto_hardware_wallet/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+
 handler404 = 'crypto_hardware_wallet.views.handler404'
