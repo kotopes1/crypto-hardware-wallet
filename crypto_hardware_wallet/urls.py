4f9aae4f5454cafa7a36bfea62a599b54e6388dc
Flow-matic <jonathonvinson22@gmail.com>
2022-10-23 18:42:27 +0000

    Added urls path in blog urls file

--- crypto_hardware_wallet/urls.py
+++ crypto_hardware_wallet/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('summernote/', include('django_summernote.urls')),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
