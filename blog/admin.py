437415e0d2726d0bfe724c6e951e81933dcd6c38
Flow-matic <jonathonvinson22@gmail.com>
2023-01-03 13:24:58 +0000

    summmernote to apps in settings and added urls for summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post
 
 
-class PostAdmin(admin.ModelAdmin):
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    For posts in blog
+    """
     list_display = ('title', 'slug', 'status', 'created_on')
-    list_filter = ('status',)
     search_fields = ['title', 'content']
-
-
-admin.site.register(Post, PostAdmin)
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
