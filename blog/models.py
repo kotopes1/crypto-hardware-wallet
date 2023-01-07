6a5b20f7bf4f66c04d4fbd5b79cfb8c46905efd4
Flow-matic <jonathonvinson22@gmail.com>
2023-01-07 15:39:14 +0000

    Added comments models for blog posts users, to models.py file

--- blog/models.py
+++ blog/models.py
@@ -13,16 +13,41 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='blog_post'
+        User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-    created_on = models.DateTimeField(auto_now_add=True)
+    excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
 
     class Meta:
         """ Sorts the posts in descending order """
-        ordering = ['-created_on']
+        ordering = ["-created_on"]
 
     def __str__(self):
         return self.title
+
+    def number_of_likes(self):
+        """
+        Handles total number of likes
+        """
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
