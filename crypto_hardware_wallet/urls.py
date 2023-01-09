7f854469be6b93fe7c86074f0e0d8466c20f459e
Flow-matic <jonathonvinson22@gmail.com>
2023-01-09 20:18:48 +0000

    Added links to drop down nav bar for blog urls

--- crypto_hardware_wallet/urls.py
+++ crypto_hardware_wallet/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-    path("", include("blog.urls"), name="blog-urls"),
+    path('blog/', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
