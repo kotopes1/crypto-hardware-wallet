a7b6818bb0cb847ab7aa66693a3482f082c58ce1
Flow-matic <jonathonvinson22@gmail.com>
2023-01-27 16:03:05 +0000

    Added docstring to class comment, A model representing a comment on a post. as an error was showing that is was missing

--- blog/models.py
+++ blog/models.py
@@ -38,6 +38,9 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
+    """
+    A model reprsenting a comment on a post.
+    """
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
                              related_name="comments")
     name = models.CharField(max_length=80)
